23f4d0bdb3d4618185aa7ba97f946abb3a788502
Ayushjo <ayushs24101@iiitnr.edu.in>
2025-07-25 20:27:46 +0530

    Update vite.config.ts

--- fe-server/vite.config.ts
+++ fe-server/vite.config.ts
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    // This makes the server accessible externally
+    host: true, 
+    
+    // Add your custom domain here
+    allowedHosts: ['stagingfedevops.iayush.com'],
+  },
 })
